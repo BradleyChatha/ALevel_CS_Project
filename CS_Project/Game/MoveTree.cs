7690b5769c9f4bf1c7a9d9a8a336d9f378709249
SealabJaster <sealabjaster@gmail.com>
2016-12-15 06:54:07 +0000

    Add Node

--- CS_Project/Game/MoveTree.cs
+++ CS_Project/Game/MoveTree.cs
@@ -16,12 +16,50 @@ namespace CS_Project.Game
     /// </summary>
     class MoveTree
     {
-        /// <summary>
-        /// A node describing a single move.
-        /// </summary>
-        class Node
-        {
 
+    }
+
+    /// <summary>
+    /// A node describing a single move.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// The hash of the board after the move was made.
+        /// </summary>
+        public readonly Hash hash;
+
+        /// <summary>
+        /// The index of what slot was changed this move.
+        /// </summary>
+        public readonly uint index;
+
+        /// <summary>
+        /// How many times this move was used in a game that was won.
+        /// </summary>
+        public uint won;
+
+        /// <summary>
+        /// How many times this move was used in a game that was lost.
+        /// </summary>
+        public uint lost;
+
+        /// <summary>
+        /// Creates a new Node
+        /// </summary>
+        /// <param name="hash">The 'Hash' of the board after the move was made.</param>
+        /// <param name="index">The index of the slot that was changed.</param>
+        /// <param name="won">How many times this move was used in a won match.</param>
+        /// <param name="lost">The opposite of 'won'</param>
+        public Node(Hash hash, uint index, uint won = 0, uint lost = 0)
+        {
+            if(hash == null)
+                throw new ArgumentNullException("hash");
+
+            this.hash  = hash;
+            this.index = index;
+            this.won   = won;
+            this.lost  = lost;
         }
     }
 }
